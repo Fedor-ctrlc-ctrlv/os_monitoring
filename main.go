5ed7733f1323e0f908ab55cb1d10a25d7ef1b44f
Fedor-ctrlc-ctrlv <woodiquiker@gmail.com>
2026-09-21 20:16:40 +0300

    Прописал функцию cpu_load

--- main.go
+++ main.go
@@ -3,11 +3,45 @@ package main
 import(
 	"fmt"
 	"time"
+	"os"
+	"strconv"
+	"strings"
+	"bufio"
 )
+
+func getCPUload() float64{
+	file,err:=os.Open("/proc/stat")
+	if err!=nil{
+		return 0
+	}
+	defer file.Close()
+
+	scanner:= bufio.NewScanner(file)
+	scanner.Scan()
+	line:=scanner.Text()
+	fields:= strings.Fields(line)
+	if len(fields)<5{
+		return 0
+	}
+
+	var total,idle uint64
+	for i:=1 ;i<len(fields);i++{
+		val,_:= strconv.ParseUint(fields[i],10,64)
+		total+=val
+		if i==4{
+			idle=val
+		}
+	}
+	if total == 0{
+		return 0
+	}
+	return float64(total - idle)/float64(total)*100
+}
+
 
 func main(){
 	curent_time:=time.Now()
-	cpu_load:=0
-	fmt.Printf("Time is - %s", curent_time.Format("15.03.2005 15:01:05"))
-	fmt.Printf("\n CPU load is %d \n",cpu_load)
+	cpu_load:=getCPUload()
+	fmt.Printf("Time is - %s", curent_time.Format("15.03.2006 15:01:05"))
+	fmt.Printf("\n CPU load is %.2f%% \n",cpu_load)
 }
